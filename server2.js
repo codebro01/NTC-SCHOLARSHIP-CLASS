aeae4f1744c3f59ae70122ebc882ffb72e038f55
codebro <samsondamilola.99@gmail.com>
2025-04-05 04:41:33 +0100

    last class updates

--- server2.js
+++ server2.js
@@ -1,33 +1,3 @@
-// const express = require('express')
-// const { createServer } = require('node:http')
-// const { join } = require('node:path')
-// const { Server } = require('socket.io')
-
-// const app = express()
-// const server = createServer(app)
-// const io = new Server(server)
-
-// app.get('/', (req, res) => {
-//   res.sendFile(join(__dirname, 'index.html'))
-// })
-
-// // io.on('connection', (socket) => {
-// //   console.log('a user connected')
-// //   socket.on('disconnect', () => {
-// //     console.log('user disconnected')
-// //   })
-// // })
-
-// io.on('connection', (socket) => {
-//   socket.on('chat message', (msg) => {
-//     console.log('message: ' + msg)
-//   })
-// })
-
-// server.listen(3000, () => {
-//   console.log('server running at http://localhost:3000')
-// })
-
 require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
@@ -102,4 +72,3 @@ const startDB = async () => {
   })
 }
 startDB()
-
